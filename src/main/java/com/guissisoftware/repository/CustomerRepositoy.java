b35355878971036e8f4fd3a4d7f632469b86743c
Henri-joel Guissi <guissi.henrijoelzeze@bluecrest.edu.gh>
2023-04-09 09:50:01 +0000

    Project structure corrected.

--- src/main/java/com/guissisoftware/repository/CustomerRepositoy.java
+++
@@ -1,8 +0,0 @@
-package com.guissisoftware.repository;
-
-import com.guissisoftware.models.CustomerEntity;
-import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
-
-public class CustomerRepositoy implements PanacheRepositoryBase<CustomerEntity, Long> {
-
-}
